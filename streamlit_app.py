879c29c2c732e75e986e4cf526ed169d32799c10
Claudemir F Franco <claudemir.franco@gmail.com>
2023-03-24 14:37:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
-import snowflake.connector
 import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
